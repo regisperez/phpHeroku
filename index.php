9b6269acee23d7c86a54488e315742f4ebf3427b
Regis Perez <regisbatistaperez@gmail.com>
2018-10-05 13:50:50 -0300

    pass

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <body>
     <?php
 
-		mysqli_connect("h40lg7qyub2umdvb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "gzz0aspvp0vex2ad", "z9uuiojkjec1ltej ", "z0b3o8f5xmle1nmz") or die(mysqli_error());
+		mysqli_connect("h40lg7qyub2umdvb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "gzz0aspvp0vex2ad", "z9uuiojkjec1ltej", "z0b3o8f5xmle1nmz") or die(mysqli_error());
 
 		echo "Connected to MySQL<br />";
 
